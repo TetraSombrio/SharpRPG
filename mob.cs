971e94de7d3b7b8ce04820298059913f81a9cdf4
WKMS4R <108081611+WKMS4R@users.noreply.github.com>
2022-06-23 19:04:08 +0300

    Add files via upload

---
+++ mob.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace mob
+{
+    class mob
+    {   
+       public string name;
+        public int hp;
+        public int attk;
+        public int xp;
+        public virtual void creator()
+        {
+       
+        }
+    }
+    class elf:mob
+    {   
+        string[] narr = new string[4] {"Эльф - бомж","Эльф - вор","Эльф - барыга","Эльф - гондон" };
+      
+        public override void creator()
+        {
+            base.creator();
+           Random random = new Random();
+            elf elf = new elf();
+            elf.name = narr[random.Next(0, 3)];
+            elf.hp = random.Next(10,70);
+            elf.attk = random.Next();
+            elf.xp = random.Next();
+            Console.WriteLine($"\nПеред вами {elf.name}\nЗдоровье противника: {elf.hp}\n");
+        }
+
+    }
+   class program
+    {
+        static void Main()
+        {   elf elf =new elf();
+            elf.creator();
+        }
+    }
+   
+
+
+
+}
